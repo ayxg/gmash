abd450a7c43e022da16855b5cc292102a594137e
Anton Yashchenko <zhymet@gmail.com>
2025-09-29 02:12:20 -0400

    - Removed unnecessary `commands.sh` ,`dispatch_parsers.sh` and `parsers.sh` scripts. The code is in the main `gmash` script. It's not large enough to justify splitting context. + `gmash mono` : rename `new` command to `sub`(subproject/subtree) + `gmash mono` :  rename command `patch` to `pull` + `gmash mono` :  subtree patch is now `mono pull` + `gmash mono` : add new `config` command parser.

--- gmash-source/def-parser/mono.sh
+++ gmash-source/def-parser/mono.sh
@@ -17,10 +17,11 @@ gmash_def_parser_mono(){
   standard_parser_setup GMASH_MONO_ARGR gmash_mono_help \
     "Usage: gmash mono <sub-command> [[args]...]"
   msg -- " "
+  msg -- "Git+GitHub monorepo workflow strategy."
   msg -- "Call [main-cmd] [sub-cmd] --help for details of each sub-command."
   msg -- " "
   msg -- "Sub-Commands:"
-  cmd project \
+  cmd sub \
     -- "Add or re-configure a sub project to the mono repo as a subtree."
   cmd pull \
     -- "Pull changes from a sub project's remote into the mono repo."
@@ -35,12 +36,72 @@ gmash_def_parser_mono(){
       -- "[$GMASH_MONO_VERSION] Display command group version."
 }
 
-gmash_def_parser_mono_patch(){
+gmash_def_parser_mono_sub(){
+  extend_parser
+  standard_parser_setup GMASH_SUBTREE_NEW_ARGR gmash_subtree_new_help \
+    "Usage: gmash mono sub <-p <subtreePrefixPath>> <-r <remoteAlias>> <-l <remoteUrl>> [-b <subtreeBranch>]"
+  msg -- " "
+  msg -- "Add or re-configure a sub project to the mono repo as a subtree."
+  msg -- " "
+  msg -- "Parameters:"
+    param GMASH_SUBTREE_NEW_PREFIX -p --prefix var:'<subtreePrefixPath>'\
+      -- "Relative path inside the parent repo where the subtree will be added. Cannot be\
+ the root path. The path must be empty or non-existent in the parent repo. gmash will deny\
+ adding a subtree to a path which already contains any files."
+     param GMASH_SUBTREE_NEW_REMOTE -r --remote var:"<remoteAlias>" \
+      -- "Remote alias to add to the parent repo, which will be refered to when pulling\
+ and pushing changes to the added subtree."
+     param GMASH_SUBTREE_NEW_URL -l --url var:"<remoteURL>" \
+      -- "Remote repository URL of the subtree to add. If not provided, gmash will\
+ attempt to find an existing GitHub repo at 'api-user/remote.git'. If no such\
+ repo exists and '--new' is passed: a new empty repo will be created at\
+ 'api-user/remote.git'."
+    param GMASH_SUBTREE_NEW_BR -b --branch var:"[monoBranch = \"main\"]"\
+      -- "Target branch of the subtree remote to pull in. Defaults to 'main'."
+    flag GMASH_SUBTREE_NEW_SQUASH -s --squash var:"" \
+      -- "Instead of merging the entire history from the subtree project, produce only\
+ a single commit that contains all the differences to merge. Then, merge that new\
+ commit into the parent repo. Note, if you add a subtree with --squash, future
+ pulls and pushes to that subtree should also be squashed."
+    flag GMASH_SUBTREE_NEW_NEW -n --new var:"" \
+      -- "Create a new github repo for the added subtree, if the target subtree remote\
+ does not exist."
+  msg -- "  "
+  msg -- "Display:"
+    standard_parser_help gmash_subtree_new_help
+    disp "GMASH_SUBTREE_NEW_VERSION" -v --version \
+      -- "[$GMASH_SUBTREE_NEW_VERSION] Display command group version."
+}
+
+gmash_def_parser_mono_pull(){
+  extend_parser
+  standard_parser_setup GMASH_SUBTREE_PATCH_ARGR gmash_subtree_patch_help \
+    "Usage: gmash subtree pull -r [repo] -b [branch]"
+  msg -- "  "
+  msg -- "Pull subtree changes to monorepo."
+  msg -- "  "
+  msg -- "Parameters:"
+    param GMASH_SUBTREE_PATCH_REMOTE -r --remote var:"<subtreeRemote>" \
+      -- "Target subtree remote alias."
+    param GMASH_SUBTREE_PATCH_BRANCH -b --branch var:"<subtreeBranch>" \
+      -- "Target subtree branch."
+    param GMASH_SUBTREE_PATCH_PREFIX -p --prefix var:"<subtreePrefixPath>"\
+      -- "Subtree prefix path in the monorepo."
+    flag GMASH_SUBTREE_PATCH_ALL -a --all var:""\
+      -- "Patch all subtrees based on gmash metadata."
+  msg -- "  "
+  msg -- "Display:"
+    standard_parser_help gmash_subtree_patch_help
+    disp "GMASH_SUBTREE_PATCH_VERSION" -v --version \
+      -- "[$GMASH_SUBTREE_PATCH_VERSION] Display command group version."
+}
+
+gmash_def_parser_mono_push(){
   extend_parser
   standard_parser_setup GMASH_MONO_PATCH_ARGR gmash_mono_patch_help \
-    "Usage: gmash mono patch -r [repo] -b [branch]"
+    "Usage: gmash mono push -r [repo] -b [branch]"
   msg -- "  "
-  msg -- "Patch a git repository with changes from another branch."
+  msg -- "Push changes in the mono repo to a sub project's remote."
   msg -- "  "
   msg -- "Parameters:"
     param GMASH_MONO_PATCH_BR -b --br var:"<monoBranch>" \
@@ -84,4 +145,27 @@ gmash_def_parser_mono_patch(){
     standard_parser_help gmash_mono_patch_help
     disp "GMASH_MONO_PATCH_VERSION" -v --version \
       -- "[$GMASH_MONO_PATCH_VERSION] Display command group version."
+}
+
+gmash_def_parser_mono_config(){
+  extend_parser
+  standard_parser_setup GMASH_SUBTREE_PATCH_ARGR gmash_subtree_patch_help \
+    "Usage: gmash subtree config -r [repo] -b [branch]"
+  msg -- "  "
+  msg -- "Configure a mono repo's remotes based on stored subproject metadata."
+  msg -- "  "
+  msg -- "Parameters:"
+    param GMASH_SUBTREE_PATCH_REMOTE -r --remote var:"<subtreeRemote>" \
+      -- "Target subtree remote alias."
+    param GMASH_SUBTREE_PATCH_BRANCH -b --branch var:"<subtreeBranch>" \
+      -- "Target subtree branch."
+    param GMASH_SUBTREE_PATCH_PREFIX -p --prefix var:"<subtreePrefixPath>"\
+      -- "Subtree prefix path in the monorepo."
+    flag GMASH_SUBTREE_PATCH_ALL -a --all var:""\
+      -- "Patch all subtrees based on gmash metadata."
+  msg -- "  "
+  msg -- "Display:"
+    standard_parser_help gmash_subtree_patch_help
+    disp "GMASH_SUBTREE_PATCH_VERSION" -v --version \
+      -- "[$GMASH_SUBTREE_PATCH_VERSION] Display command group version."
 }
